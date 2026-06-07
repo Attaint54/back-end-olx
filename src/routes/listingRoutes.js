440ad80c38f92702dd0ff51b5330bdb5d209d540
Muhammad-Asif-Ansari <muhammadasifansari101@gmail.com>
2026-06-07 15:45:19 +0500

    new

--- src/routes/listingRoutes.js
+++ src/routes/listingRoutes.js
@@ -7,7 +7,7 @@ router.get("/", listingController.getListings);
 router.get("/search", listingController.searchListings);
 router.get("/category/:category", listingController.getListingsByCategory);
 router.get("/:id", listingController.getListingById);
-router.post("/add", upload.single("image"), listingController.createListing);
+router.post("/", upload.single("image"), listingController.createListing);
 router.put("/:id", upload.single("image"), listingController.updateListing);
 router.delete("/:id", listingController.deleteListing);
 
